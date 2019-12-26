93db810bd77164fd9725008d5bbd71816735cbeb
israphial <israphial@users.noreply.github.com>
2019-12-26 09:39:43 -0500

    testing class-based color profile for easier profile creations

--- PiLantern v0.py
+++ PiLantern v0.py
@@ -17,7 +17,7 @@ def red():
 def green():
     green_led.pulse(n=1, fade_in_time=1.5, fade_out_time=1.5)
     
-def Lantern():
+def slow():
     t1 = Timer(0, blue)
     t2 = Timer(1, red)
     t3 = Timer(2, green)
@@ -25,5 +25,30 @@ def Lantern():
     t2.start()
     t3.start()
 
+class fast():
 
-button.when_pressed = Lantern()
+    def blue():
+        blue_led.pulse(n=1, fade_in_time=0.8, fade_out_time=0.8)
+    
+    def red():
+        red_led.pulse(n=1, fade_in_time=0.8, fade_out_time=0.8)
+    
+    def green():
+        green_led.pulse(n=1, fade_in_time=0.8, fade_out_time=0.8)
+
+    t1 = Timer(0, blue)
+    t2 = Timer(1, red)
+    t3 = Timer(2, green)
+
+for _ in range(10):
+    fast
+
+
+#button.when_pressed = Lantern()
+
+#function = generates a random number every time via lambda? 
+#the idea is to generate a random number between 5 and 15 seconds, and 
+#put that n into a threading timer. idk how to do this though. 
+
+#a timer must be ran every x seconds, where x is random, and upon the
+#timer running, a new random n must be generated, and Lantern called.
