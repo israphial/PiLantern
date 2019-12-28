a5934f0448fe9f8bb261d50a830b052d84e28e62
israphial <israphial@users.noreply.github.com>
2019-12-28 10:40:23 -0500

    added a few color profile functions

--- PiLantern v0.py
+++ PiLantern v0.py
@@ -8,16 +8,17 @@ red_led = PWMLED(6)
 green_led = PWMLED(13)
 button = Button(2)
 
-def blue():
-    blue_led.pulse(n=1, fade_in_time=1.5, fade_out_time=1.5)
+def slow_unsynced():
+
+    def blue():
+        blue_led.pulse(n=1, fade_in_time=1.5, fade_out_time=1.5)
     
-def red():
-    red_led.pulse(n=1, fade_in_time=1.5, fade_out_time=1.5)
+    def red():
+        red_led.pulse(n=1, fade_in_time=1.5, fade_out_time=1.5)
     
-def green():
-    green_led.pulse(n=1, fade_in_time=1.5, fade_out_time=1.5)
+    def green():
+        green_led.pulse(n=1, fade_in_time=1.5, fade_out_time=1.5)
     
-def slow():
     t1 = Timer(0, blue)
     t2 = Timer(1, red)
     t3 = Timer(2, green)
@@ -25,30 +26,40 @@ def slow():
     t2.start()
     t3.start()
 
-class fast():
+def fast_synced():
 
     def blue():
-        blue_led.pulse(n=1, fade_in_time=0.8, fade_out_time=0.8)
+        blue_led.pulse(n=6, fade_in_time=0.6, fade_out_time=0.6)
     
     def red():
-        red_led.pulse(n=1, fade_in_time=0.8, fade_out_time=0.8)
+        red_led.pulse(n=6, fade_in_time=0.6, fade_out_time=0.6)
     
     def green():
-        green_led.pulse(n=1, fade_in_time=0.8, fade_out_time=0.8)
+        green_led.pulse(n=6, fade_in_time=0.6, fade_out_time=0.6)
 
     t1 = Timer(0, blue)
-    t2 = Timer(1, red)
-    t3 = Timer(2, green)
+    t2 = Timer(0, red)
+    t3 = Timer(0, green)
+    t1.start()
+    t2.start()
+    t3.start()
 
-for _ in range(10):
-    fast
+def single_fast_blink():
 
+    def blue():
+        blue_led.pulse(n=1, fade_in_time=0.4, fade_out_time=0.4)
+    
+    def red():
+        red_led.pulse(n=1, fade_in_time=0.4, fade_out_time=0.4)
+    
+    def green():
+        green_led.pulse(n=1, fade_in_time=0.4, fade_out_time=0.4)
 
-#button.when_pressed = Lantern()
+    t1 = Timer(0, blue)
+    t2 = Timer(0, red)
+    t3 = Timer(0, green)
+    t1.start()
+    t2.start()
+    t3.start()
 
-#function = generates a random number every time via lambda? 
-#the idea is to generate a random number between 5 and 15 seconds, and 
-#put that n into a threading timer. idk how to do this though. 
-
-#a timer must be ran every x seconds, where x is random, and upon the
-#timer running, a new random n must be generated, and Lantern called.
+#button.when_pressed = fast
